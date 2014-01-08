7ba577ba9df7a2d87e11801e7e8bec2cb3bba96b
Josh Sklar <jsklar@detroitlabs.com>
2014-01-08 12:03:51 -0500

    added UI

--- TTTAttributedLabelBug/TTTALBViewController.m
+++ TTTAttributedLabelBug/TTTALBViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "TTTALBViewController.h"
+#import <TTTAttributedLabel/TTTAttributedLabel.h>
 
 @interface TTTALBViewController ()
+
+@property (strong, nonatomic) UILabel *uiLabel;
+@property (strong, nonatomic) TTTAttributedLabel *tttAttributedLabel;
 
 @end
 
@@ -17,13 +21,49 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    CGFloat width = self.view.bounds.size.width;
+    CGFloat height = 30;
+    
+    self.uiLabel = [[UILabel alloc]
+                                 initWithFrame:CGRectMake(0, height, width, height)];
+    [self.uiLabel setText:@"This is a UILabel"];
+    [self.view addSubview:self.uiLabel];
+    
+    self.tttAttributedLabel = [[TTTAttributedLabel alloc]
+                                    initWithFrame:CGRectMake(0, height * 2, width, height)];
+    [self.tttAttributedLabel setText:@"This is a TTTAttributedLabel"];
+    [self.view addSubview:self.tttAttributedLabel];
+    
+    UIButton *changeToBold = [[UIButton alloc]
+                              initWithFrame:CGRectMake(0, height * 3, width, height)];
+    [changeToBold setTitle:@"Change font to bold" forState:UIControlStateNormal];
+    [changeToBold addTarget:self
+                     action:@selector(didTapChangeToBold:)
+           forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:changeToBold];
+    
+    UIButton *changeToItalic = [[UIButton alloc]
+                                initWithFrame:CGRectMake(0, height * 4, width, height)];
+    [changeToItalic setTitle:@"Change font to italic" forState:UIControlStateNormal];
+    [changeToItalic addTarget:self
+                       action:@selector(didTapChangeToItalic:)
+             forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:changeToItalic];
 }
 
-- (void)didReceiveMemoryWarning
+- (void)didTapChangeToBold:(UIButton*)sender
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    UIFont *font = [UIFont boldSystemFontOfSize:15];
+    [self.uiLabel setFont:font];
+    [self.tttAttributedLabel setFont:font];
 }
+
+- (void)didTapChangeToItalic:(UIButton*)sender
+{
+    UIFont *font = [UIFont italicSystemFontOfSize:15];
+    [self.uiLabel setFont:font];
+    [self.tttAttributedLabel setFont:font];
+}s
 
 @end
