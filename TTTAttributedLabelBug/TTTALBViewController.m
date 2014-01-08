211e01672313f93dbd781f450f3e9de891a1e2a5
Josh Sklar <jsklar@detroitlabs.com>
2014-01-08 12:18:38 -0500

    refactor

--- TTTAttributedLabelBug/TTTALBViewController.m
+++ TTTAttributedLabelBug/TTTALBViewController.m
@@ -9,10 +9,21 @@
 #import "TTTALBViewController.h"
 #import <TTTAttributedLabel/TTTAttributedLabel.h>
 
+#define kWidth      self.view.bounds.size.width
+#define kHeight     30
+
 @interface TTTALBViewController ()
 
 @property (strong, nonatomic) UILabel *uiLabel;
 @property (strong, nonatomic) TTTAttributedLabel *tttAttributedLabel;
+
+- (void)didTapChangeToBold:(UIButton*)sender;
+- (void)didTapChangeToItalic:(UIButton*)sender;
+- (void)didTapChangeText:(UIButton*)sender;
+
+- (void)setupUILabel;
+- (void)setupTTTAttributedLabel;
+- (void)setupButtons;
 
 @end
 
@@ -22,43 +33,12 @@
 {
     [super viewDidLoad];
     
-    CGFloat width = self.view.bounds.size.width;
-    CGFloat height = 30;
-    
-    self.uiLabel = [[UILabel alloc]
-                                 initWithFrame:CGRectMake(0, height, width, height)];
-    [self.uiLabel setText:@"This is a UILabel"];
-    [self.view addSubview:self.uiLabel];
-    
-    self.tttAttributedLabel = [[TTTAttributedLabel alloc]
-                                    initWithFrame:CGRectMake(0, height * 2, width, height)];
-    [self.tttAttributedLabel setText:@"This is a TTTAttributedLabel"];
-    [self.view addSubview:self.tttAttributedLabel];
-    
-    UIButton *changeToBold = [[UIButton alloc]
-                              initWithFrame:CGRectMake(0, height * 3, width, height)];
-    [changeToBold setTitle:@"Change font to bold" forState:UIControlStateNormal];
-    [changeToBold addTarget:self
-                     action:@selector(didTapChangeToBold:)
-           forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:changeToBold];
-    
-    UIButton *changeToItalic = [[UIButton alloc]
-                                initWithFrame:CGRectMake(0, height * 4, width, height)];
-    [changeToItalic setTitle:@"Change font to italic" forState:UIControlStateNormal];
-    [changeToItalic addTarget:self
-                       action:@selector(didTapChangeToItalic:)
-             forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:changeToItalic];
-    
-    UIButton *changeText = [[UIButton alloc]
-                            initWithFrame:CGRectMake(0, height * 5, width, height)];
-    [changeText setTitle:@"Change the text" forState:UIControlStateNormal];
-    [changeText addTarget:self
-                       action:@selector(didTapChangeText:)
-             forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:changeText];
+    [self setupUILabel];
+    [self setupTTTAttributedLabel];
+    [self setupButtons];
 }
+
+#pragma mark - Internal methods
 
 - (void)didTapChangeToBold:(UIButton*)sender
 {
@@ -79,6 +59,49 @@
     NSString *text = @"This is some new fresh text";
     [self.uiLabel setText:text];
     [self.tttAttributedLabel setText:text];
+}
+
+- (void)setupUILabel
+{
+    self.uiLabel = [[UILabel alloc]
+                    initWithFrame:CGRectMake(0, kHeight, kWidth, kHeight)];
+    [self.uiLabel setText:@"This is a UILabel"];
+    [self.view addSubview:self.uiLabel];
+}
+
+- (void)setupTTTAttributedLabel
+{
+    self.tttAttributedLabel = [[TTTAttributedLabel alloc]
+                               initWithFrame:CGRectMake(0, kHeight * 2, kWidth, kHeight)];
+    [self.tttAttributedLabel setText:@"This is a TTTAttributedLabel"];
+    [self.view addSubview:self.tttAttributedLabel];
+}
+
+- (void)setupButtons
+{
+    UIButton *changeToBold = [[UIButton alloc]
+                              initWithFrame:CGRectMake(0, kHeight * 3, kWidth, kHeight)];
+    [changeToBold setTitle:@"Change font to bold" forState:UIControlStateNormal];
+    [changeToBold addTarget:self
+                     action:@selector(didTapChangeToBold:)
+           forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:changeToBold];
+    
+    UIButton *changeToItalic = [[UIButton alloc]
+                                initWithFrame:CGRectMake(0, kHeight * 4, kWidth, kHeight)];
+    [changeToItalic setTitle:@"Change font to italic" forState:UIControlStateNormal];
+    [changeToItalic addTarget:self
+                       action:@selector(didTapChangeToItalic:)
+             forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:changeToItalic];
+    
+    UIButton *changeText = [[UIButton alloc]
+                            initWithFrame:CGRectMake(0, kHeight * 5, kWidth, kHeight)];
+    [changeText setTitle:@"Change the text" forState:UIControlStateNormal];
+    [changeText addTarget:self
+                   action:@selector(didTapChangeText:)
+         forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:changeText];
 }
 
 @end
